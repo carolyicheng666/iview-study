df047d8916a52b73273a843fed69f36baf4810c3
Yicheng <790496165@qq.com>
2018-06-20 19:41:43 +0800

    :sparkles: feat: add form

--- src/router.js
+++ src/router.js
@@ -1,18 +1,18 @@
-const routers = [{
-    path: '/',
-    meta: {
-      title: ''
-    },
-    component: (resolve) => require(['./views/index.vue'], resolve)
+const index = [{
+  path: '/',
+  meta: {
+    title: ''
   },
-  {
+  component: (resolve) => require(['./views/index.vue'], resolve)
+}];
+
+const basic = [{
     path: '/basic/grid',
     meta: {
       title: 'grid'
     },
     component: (resolve) => require(['./views/basic/grid.vue'], resolve)
-  },
-  {
+  }, {
     path: '/basic/layout',
     meta: {
       title: 'layout'
@@ -27,4 +27,51 @@ const routers = [{
     component: (resolve) => require(['./views/basic/button.vue'], resolve)
   }
 ];
+
+const form = [{
+    path: '/form/input',
+    meta: {
+      title: 'input'
+    },
+    component: (resolve) => require(['./views/form/input.vue'], resolve)
+  },
+  {
+    path: '/form/radio',
+    meta: {
+      title: 'radio'
+    },
+    component: (resolve) => require(['./views/form/radio.vue'], resolve)
+  },
+  {
+    path: '/form/checkbox',
+    meta: {
+      title: 'checkbox'
+    },
+    component: (resolve) => require(['./views/form/checkbox.vue'], resolve)
+  },
+  {
+    path: '/form/switch',
+    meta: {
+      title: 'switch'
+    },
+    component: (resolve) => require(['./views/form/switch.vue'], resolve)
+  },
+  {
+    path: '/form/select',
+    meta: {
+      title: 'select'
+    },
+    component: (resolve) => require(['./views/form/select.vue'], resolve)
+  },
+  {
+    path: '/form/rate',
+    meta: {
+      title: 'rate'
+    },
+    component: (resolve) => require(['./views/form/rate.vue'], resolve)
+  }
+];
+
+const routers = [...index, ...basic, ...form];
+
 export default routers;
